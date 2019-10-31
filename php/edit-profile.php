c16268cb874e55ed6757453f45761b6b2fc671e5
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-10-31 19:15:01 +0700

    commit

--- php/edit-profile.php
+++ php/edit-profile.php
@@ -6,4 +6,6 @@ $phone = $_POST["phone"];
 $email = $_POST["email"];
 $city = $_POST["city"];
 $profilePicture = $_POST["profile_picture"];
-$c->query("UPDATE customer SET name='" . $name . "', phone='" . $phone . "', email='" . $email . "', city='" . $city . "', profile_picture='" . $profilePicture . "' WHERE id=" . $id);
+$sql = "UPDATE customer SET name='" . $name . "', phone='" . $phone . "', email='" . $email . "', city='" . $city . "', picture='" . $profilePicture . "' WHERE id=" . $id;
+$c->query($sql);
+echo $sql;
--- php/get-by-id-name-string.php
+++ php/get-by-id-name-string.php
@@ -4,7 +4,8 @@ $name = $_POST["name"];
 $idName = $_POST["id_name"];
 $id = $_POST["id"];
 $items = [];
-$results = $c->query("SELECT * FROM " . $name . " WHERE " . $idName . "='" . $id . "'");
+$sql = "SELECT * FROM " . $name . " WHERE " . $idName . "='" . $id . "'";
+$results = $c->query($sql);
 if ($results && $results->num_rows > 0) {
 	while ($row = $results->fetch_assoc()) {
 		array_push($items, $row);
